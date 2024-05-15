8e2f36f9bbee08f52aded3ff2f139dd2b16304d9
Michelle Ma <144358728+michellehma@users.noreply.github.com>
2024-05-15 10:34:58 -0400

    fixed more stuff

--- src/components/Dropdown/DropdownEdit.jsx
+++ src/components/Dropdown/DropdownEdit.jsx
@@ -4,30 +4,11 @@ import DropdownEditAcct from "../DropdownContent/DropdownEditAcct";
 import "./Dropdown.css";
 import { useState } from "react";
 
-const DropdownEdit = ({buttonText, content}) => {
-
-    const [open, setOpen] = useState(false);
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-
-    const toggleDropdown = () => {
-        setOpen((open) => !open);
-        return fetch("http://localhost:5000/display_user", {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                    }
-            }).then((res) => 
-            res.json().then((data) => {
-                setUsername(data.username);
-                setEmail(data.email);
-            })
-        );
-    };
+const DropdownEdit = ({buttonText, content, click, open, username, email}) => {
 
     return (
         <div className="dropdown">
-            <DropdownButton toggle={toggleDropdown} open={open}>
+            <DropdownButton toggle={click} open={open}>
                 {buttonText}            
             </DropdownButton>
             <DropdownEditAcct open={open} username1={username} email1={email}>
