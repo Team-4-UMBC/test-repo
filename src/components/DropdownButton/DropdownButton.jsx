7871b01827c41925e67f96ce8d64a530ea397375
Michelle Ma <144358728+michellehma@users.noreply.github.com>
2024-05-17 16:39:52 -0400

    new fetches

--- src/components/DropdownButton/DropdownButton.jsx
+++ src/components/DropdownButton/DropdownButton.jsx
@@ -2,12 +2,15 @@ import "./DropdownButton.css";
 import "./arrow-down-sign-to-navigate.png";
 
 const DropdownButton = ({children, open, toggle}) => {
-    return <div onClick={toggle} className={`dropdown-btn ${open ? "button-open" : null}`}> 
-        {children} 
-        <span>
-            <img src={require("./arrow-down-sign-to-navigate.png") } style={{width:"12", height:"12"}}></img>
-        </span>
-    </div>;
+    
+        return (
+            <div onClick={toggle} className={`dropdown-btn ${open ? "button-open" : null}`}> 
+                {children} 
+                    <span>
+                        <img src={require("./arrow-down-sign-to-navigate.png") } style={{width:"12", height:"12"}}></img>
+                    </span>
+            </div>
+        );
 };
 
 export default DropdownButton;
